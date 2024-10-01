f0c42c28cd060b726a91284fcee87e3fcc9564a4
Caden Parsons <cjparsons@uncg.edu>
2024-10-01 10:59:35 -0400

    Starting code, do as you wish

---
+++ src/main/java/com/_0/PartyPulse/PartyPulseApplication.java
@@ -0,0 +1,13 @@
+package com._0.PartyPulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyPulseApplication.class, args);
+	}
+
+}
